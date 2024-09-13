005f6356dcbee6179abdbbb79deeee8913273513
Faiz-Rehman2 <FAIZREHMAN9o11@gmail.com>
2024-09-13 06:55:54 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Products from './components/Products.jsx'
+import Navbar from './components/Navbar.jsx'
 
 
 createRoot(document.getElementById('root')).render(
    <App />
+   {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "",
+          element: <Navbar />
+        },
+        {
+          path: "about",
+          element: <Products />,}
+        ]
+      }
 
 
 )
